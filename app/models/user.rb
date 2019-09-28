f96c75cc75e3d0dde42751136a7730c3079a56da
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-28 21:26:33 +0100

    Write User methods for remember, forget, send login email, attempt to login, generate token and digest creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,35 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  attr_accessor :session_token
+  before_save :downcase_email
+
+  def downcase_email
+    self.email.downcase
+  end
+
+  def send_login_email
+    SessionsMailer.login_validation(self).deliver_now
+  end
+
+  def User.digest(token)
+    Digest::SHA1.hexdigest(token)
+  end
+
+  def attempt_to_login
+    self.session_token = User.generate_token
+    update_attribute(:session_digest, User.digest(session_token))
+  end
+
+  def User.generate_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.generate_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
