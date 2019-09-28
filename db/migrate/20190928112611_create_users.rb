37026aaf1a73d979e742d23168f164240377ba67
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-28 21:27:33 +0100

    Write users migration

--- db/migrate/20190928112611_create_users.rb
+++ db/migrate/20190928112611_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :email
+      t.string :email, null: false
       t.string :session_digest
-      t.string :username
-      t.string :name
+      t.string :username, null: false
+      t.string :name, null: falsex
 
       t.timestamps
     end
