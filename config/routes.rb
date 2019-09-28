bb33e66b731827155cff7c145948d3ef90186d0e
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-28 21:27:16 +0100

    Write routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: %i[create edit update destroy]
+  resources :sessions, only: %i[edit]
+  root 'static_pages#landing_page'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/profile', to: 'users#show'
+  delete '/logout', to: 'sessions#destroy'
+
 end
