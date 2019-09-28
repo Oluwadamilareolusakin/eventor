507bd44daf635ca426c499f64cf29dc56c546618
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-28 23:48:50 +0100

    Move session aux methods to app controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,31 +1,3 @@
 module ApplicationHelper
-  def login(user)
-    session[:user_id] = @user.id
-  end
-
-  def current_user
-    if user_id = session[:user_id]
-      @current_user ||= User.find_by(id: user_id) 
-    elsif user_id = cookies.signed[:user_id]
-      user = User.find_by(id: user_id)
-      login(user)
-    end
-  end
-
-  def remember(user)
-    user.remember
-    cookies.signed.permanent[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
-  def forget(user)
-    user.forget
-    cookies.delete(:remember_token)
-    cookies.delete(:user_id)
-  end
-
-  def logout
-    forget(current_user)
-    session.delete(:user_id)
-  end
+  
 end
