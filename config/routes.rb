a509f73d14a5a1a65840329017672ce8560891a3
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-29 12:27:44 +0100

    Add event resources to routes, add email index for users, create events table

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[create edit update destroy show]
   resources :sessions, only: %i[edit]
+  resources :events
+
   root 'static_pages#landing_page'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_28_222843) do
+ActiveRecord::Schema.define(version: 2019_09_29_103239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.date "date", null: false
+    t.time "time", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -24,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_09_28_222843) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "remember_digest"
     t.datetime "session_created_at"
+    t.index ["email"], name: "index_users_on_email"
   end
 
 end
