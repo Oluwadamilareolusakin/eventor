f0dba13591abc141b836549965d112a8b5c13766
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-29 12:31:22 +0100

    Model events and create controller actions with matching views

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,47 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: %i[show update edit destroy]
+  before_action :log_in_user, only: %i[new create edit update destroy]
+  
+  def index
+    @events = Events.all
+  end
+
+  def show
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @user = current_user  
+    @event = @user.events.build(event_params)
+    if @event.save
+      flash[:success] = "#{@event.title} was successfully created! Start inviting people."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @event.update(event_params)
+    if @event.save
+      flash[:success] = "#{@event.title} was successfully updated!"
+      redirect_to @event
+    else
+      render :edit
+    end
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:title, :description, :date, :time)
+    end
+
+    def set_event
+      @event = Event.find(params[:id])
+    end
+end
---
+++ db/migrate/20190929103239_add_null_constraint_to_events.rb
@@ -0,0 +1,8 @@
+class AddNullConstraintToEvents < ActiveRecord::Migration[6.0]
+  def change
+    change_column_null :events, :title, false
+    change_column_null :events, :date, false
+    change_column_null :events, :time, false
+    change_column_null :events, :description, false
+  end
+end
