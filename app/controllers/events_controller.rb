1c16e9fcb71eab3a17abd1c004f1009ceeea124a
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-09-29 12:44:48 +0100

    Add user id to permited events attributes in event_params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -38,7 +38,7 @@ class EventsController < ApplicationController
 
   private
     def event_params
-      params.require(:event).permit(:title, :description, :date, :time)
+      params.require(:event).permit(:title, :description, :date, :time, :user_id)
     end
 
     def set_event
