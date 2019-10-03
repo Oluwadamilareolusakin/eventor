f871ac40547b5c364862bd71f9af4faec37b16c3
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-03 16:49:19 +0100

    Add sphinx components to events stylesheet, style event show page and events section of dashboard, upcoming events and past events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -4,12 +4,4 @@ module EventsHelper
   def event_time(event)
     event.event_time.strftime('%H:%M')
   end
-
-  def attendees(attendees)
-    if attendees.any?
-      attendees.each do |attendee|
-        attendee
-      end
-    end
-  end
 end
