e74975ae807820a456815c44adae0297eae49fd9
Tolulope Oluwadamilare Olusakin <oluwadamilare_olusakin@yahoo.com>
2019-10-04 23:06:26 +0100

    Let the TSE into the app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,9 +19,11 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = 'Please check your email to login'
+      login(@user)
       @user.attempt_to_login
       @user.send_login_email
-      redirect_to signup_confirmation_path(@user.name)
+      # redirect_to signup_confirmation_path(@user.name)
+      redirect_to root_path
     else
       render :new
     end
